1fe0a824814346d799267fd64c1454c66a607258
mike.makhovyk <m.makhovyk@gmail.com>
2024-12-28 15:54:17 +0200

    Solve Merge two lists problem

--- LinkedList/LinkedList.playground/Contents.swift
+++ LinkedList/LinkedList.playground/Contents.swift
@@ -18,6 +18,53 @@ reverseLinkedList([1, 2, 3, 4, 5].toListNode)?.toArray ?? []
 reverseLinkedList([1, 2].toListNode)?.toArray ?? []
 reverseLinkedList([].toListNode)?.toArray ?? []
 
+// MARK: - 21. Merge Two Sorted Lists [Easy]
+// https://leetcode.com/problems/merge-two-sorted-lists/
+// Merge two sorted linked lists and return it as a sorted list. The list should be made by splicing together the nodes of the first two lists.
+
+func mergeTwoLists(_ list1: ListNode?, _ list2: ListNode?) -> ListNode? {
+  if list1 == nil && list2 == nil { return nil }
+  guard var list1 else { return list2 }
+  guard var list2 else { return list1 }
+
+  // dummy head to have something to attach the first
+  var head = ListNode()
+  var current = head
+
+  while true {
+    if list1.val < list2.val {
+      let newNode = ListNode(list1.val)
+      current.next = newNode
+      current = newNode
+
+      // in case we have reached the end of the first list, just append the rest of the second
+      guard let next = list1.next else {
+        current.next = list2
+        current = list2
+        break
+      }
+      list1 = next
+    } else {
+      let newNode = ListNode(list2.val)
+      current.next = newNode
+      current = newNode
+
+      // in case we have reached the end of the second list, just append the rest of the first
+      guard let next = list2.next else {
+        current.next = list1
+        current = list1
+        break
+      }
+      list2 = next
+    }
+  }
+  return head.next
+}
+
+mergeTwoLists([1, 2, 4].toListNode, [1, 3, 4].toListNode)?.toArray ?? []
+mergeTwoLists([].toListNode, [].toListNode)?.toArray ?? []
+mergeTwoLists([].toListNode, [0].toListNode)?.toArray ?? []
+
 
 // MARK: - 2. Add Two Numbers [Medium]
 // https://leetcode.com/problems/add-two-numbers/
