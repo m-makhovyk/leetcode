a0b4982c8b1f230ba599bde04ee1cef7a81a590b
mike.makhovyk <m.makhovyk@gmail.com>
2024-12-28 12:51:16 +0200

    Solve Reverse linked list problem

--- LinkedList/LinkedList.playground/Contents.swift
+++ LinkedList/LinkedList.playground/Contents.swift
@@ -1,1 +1,19 @@
+import Foundation
 
+// MARK: - 206. Reverse Linked List [Easy]
+// https://leetcode.com/problems/reverse-linked-list/
+// Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+func reverseLinkedList(_ head: ListNode?) -> ListNode? {
+  guard var head else { return nil }
+  var tail = ListNode(head.val)
+  while let next = head.next {
+    tail = ListNode(next.val, tail)
+    head = next
+  }
+  return tail
+}
+
+reverseLinkedList([1, 2, 3, 4, 5].toListNode)?.toArray ?? []
+reverseLinkedList([1, 2].toListNode)?.toArray ?? []
+reverseLinkedList([].toListNode)?.toArray ?? []
