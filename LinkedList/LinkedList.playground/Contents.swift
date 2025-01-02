665129f7f293e66829c635c0e1789f9918a03d41
mike.makhovyk <m.makhovyk@gmail.com>
2025-01-02 21:06:27 +0200

    reorder problems in ascending order

--- LinkedList/LinkedList.playground/Contents.swift
+++ LinkedList/LinkedList.playground/Contents.swift
@@ -1,100 +1,72 @@
 import Foundation
 
-// MARK: - 206. Reverse Linked List [Easy]
-// https://leetcode.com/problems/reverse-linked-list/
-// Given the head of a singly linked list, reverse the list, and return the reversed list.
+// MARK: - 2. Add Two Numbers [Medium]
+// https://leetcode.com/problems/add-two-numbers/
+// You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit.
+// Add the two numbers and return the sum as a linked list.
+// You may assume the two numbers do not contain any leading zero, except the number 0 itself.
 
-func reverseLinkedList(_ head: ListNode?) -> ListNode? {
-  guard var head else { return nil }
-  var tail = ListNode(head.val)
-  while let next = head.next {
-    tail = ListNode(next.val, tail)
-    head = next
-  }
-  return tail
-}
+func addTwoNumbers(_ l1: ListNode?, _ l2: ListNode?) -> ListNode? {
+  guard var l1, var l2 else { return nil }
 
-reverseLinkedList([1, 2, 3, 4, 5].toListNode)?.toArray ?? []
-reverseLinkedList([1, 2].toListNode)?.toArray ?? []
-reverseLinkedList([].toListNode)?.toArray ?? []
-
-// MARK: - 203. Remove Linked List Elements [Easy]
-// https://leetcode.com/problems/remove-linked-list-elements/
-// Given the head of a linked list and an integer val, remove all the nodes of the linked list that has Node.val == val, and return the new head.
-
-func removeLinkedListElements(_ head: ListNode?, _ val: Int) -> ListNode? {
-  guard var head else { return nil }
-
+  var leftOver = (l1.val + l2.val) / 10
+  let head = ListNode((l1.val + l2.val) % 10)
   var current = head
-  while let next = current.next {
-    // skip the next node if it has the value we want to remove
-    if next.val == val {
-      current.next = next.next
-    } else {
-      current = next
-    }
+
+  while l1.next != nil || l2.next != nil {
+    let value = (l1.next?.val ?? 0) + (l2.next?.val ?? 0) + leftOver
+
+    let newNode = ListNode(value % 10)
+    current.next = newNode
+    current = newNode
+
+    l1 = l1.next ?? l1
+    l2 = l2.next ?? l2
+    leftOver = value / 10
   }
-  // check if first node has the value we want to remove
-  return head.val == val ? head.next : head
+
+  if leftOver > 0 {
+    let newNode = ListNode(leftOver)
+    current.next = newNode
+    current = newNode
+  }
+
+  return head
 }
 
-removeLinkedListElements([1, 2, 6, 3, 4, 5, 6].toListNode, 6)?.toArray ?? []
-removeLinkedListElements([].toListNode, 1)?.toArray ?? []
-removeLinkedListElements([7, 7, 7, 7].toListNode, 7)?.toArray ?? []
+addTwoNumbers([2, 4, 3].toListNode, [5, 6, 4].toListNode)?.toArray ?? []
+addTwoNumbers([0].toListNode, [0].toListNode)?.toArray ?? []
+addTwoNumbers([9, 9, 9, 9, 9, 9, 9].toListNode, [9, 9, 9, 9].toListNode)?.toArray ?? []
 
-// MARK: - 234. Palindrome Linked List [Easy]
-// https://leetcode.com/problems/palindrome-linked-list/
-// Given the head of a singly linked list, return true if it is a palindrome or false otherwise.
+// MARK: - 19. Remove Nth Node From End of List [Medium]
+// https://leetcode.com/problems/remove-nth-node-from-end-of-list/
+// Given the head of a linked list, remove the nth node from the end of the list and return its head.
 
-func palindromeLinkedList(_ head: ListNode?) -> Bool {
-  guard let head else { return true }
+func removeNthFromEnd(_ head: ListNode?, _ n: Int) -> ListNode? {
+  // if there is only one element in the list, remove it
+  guard head?.next != nil else { return nil }
 
-  // reversing the list to get the tail
-  var current: ListNode? = head
-  var tail: ListNode? = ListNode(head.val)
+  var count = 0
 
-  while let next = current?.next {
-    tail = ListNode(next.val, tail)
-    current = next
-  }
-
-  // comparing the original list with the reversed one, for palindrome they should be the same
-  current = head
-  while tail != nil {
-    if current!.val != tail!.val {
-      return false
-    }
-    current = current!.next
-    tail = tail!.next
-  }
-
-  return true
-}
-
-palindromeLinkedList([1, 2, 2, 1].toListNode)
-palindromeLinkedList([1, 2].toListNode)
-
-// MARK: - 83. Remove Duplicates from Sorted List [Easy]
-// https://leetcode.com/problems/remove-duplicates-from-sorted-list/
-// Given the head of a sorted linked list, delete all duplicates such that each element appears only once. Return the linked list sorted as well.
-
-func removeDuplicatesFromSortedList(_ head: ListNode?) -> ListNode? {
-  guard var head else { return nil }
-
-  var storage: [Int: Int] = [:]
-  var current: ListNode? = head
-
-  // iterating thorugh the list to count the ocurrences of each value
+  // iterating through the list to get the total number of elements
+  var current = head
   while current != nil {
-    storage[current!.val, default: 0] += 1
+    count += 1
     current = current!.next
   }
 
+  var targetIndex = count - n
+  // if target index is zero, skip first element
+  if targetIndex == 0 {
+    return head?.next
+  }
+
+  var index = 0
   current = head
   while current != nil {
-    if storage[current!.val, default: 0] > 1 {
+    index += 1
+    if index == targetIndex {
       current!.next = current!.next?.next
-      storage[current!.val, default: 1] -= 1
     } else {
       current = current!.next
     }
@@ -103,8 +75,9 @@ func removeDuplicatesFromSortedList(_ head: ListNode?) -> ListNode? {
   return head
 }
 
-removeDuplicatesFromSortedList([1, 1, 2].toListNode)?.toArray ?? []
-removeDuplicatesFromSortedList([1, 1, 2, 3, 3].toListNode)?.toArray ?? []
+removeNthFromEnd([1, 2, 3, 4, 5].toListNode, 2)?.toArray ?? []
+removeNthFromEnd([1].toListNode, 1)?.toArray ?? []
+removeNthFromEnd([1, 2].toListNode, 1)?.toArray ?? []
 
 // MARK: - 21. Merge Two Sorted Lists [Easy]
 // https://leetcode.com/problems/merge-two-sorted-lists/
@@ -153,35 +126,53 @@ mergeTwoLists([1, 2, 4].toListNode, [1, 3, 4].toListNode)?.toArray ?? []
 mergeTwoLists([].toListNode, [].toListNode)?.toArray ?? []
 mergeTwoLists([].toListNode, [0].toListNode)?.toArray ?? []
 
-// MARK: - 19. Remove Nth Node From End of List [Medium]
-// https://leetcode.com/problems/remove-nth-node-from-end-of-list/
-// Given the head of a linked list, remove the nth node from the end of the list and return its head.
+// MARK: - 24. Swap Nodes in Pairs [Medium]
+// https://leetcode.com/problems/swap-nodes-in-pairs/
+// Given a linked list, swap every two adjacent nodes and return its head.
+// You must solve the problem without modifying the values in the list's nodes (i.e., only nodes themselves may be changed.)
 
-func removeNthFromEnd(_ head: ListNode?, _ n: Int) -> ListNode? {
-  // if there is only one element in the list, remove it
-  guard head?.next != nil else { return nil }
+func swapPairs(_ head: ListNode?) -> ListNode? {
+  guard var current = head, current.next != nil else { return head }
 
-  var count = 0
+  while let next = current.next {
+    // swapping values without temp variable
+    current.val += next.val
+    next.val = current.val - next.val
+    current.val -= next.val
 
-  // iterating through the list to get the total number of elements
-  var current = head
+    // hopping over the next node
+    current = next.next ?? next
+  }
+
+  return head
+}
+
+swapPairs([1, 2, 3, 4].toListNode)?.toArray ?? []
+swapPairs([1, 2, 3].toListNode)?.toArray ?? []
+swapPairs([1].toListNode)?.toArray ?? []
+swapPairs([].toListNode)?.toArray ?? []
+
+// MARK: - 83. Remove Duplicates from Sorted List [Easy]
+// https://leetcode.com/problems/remove-duplicates-from-sorted-list/
+// Given the head of a sorted linked list, delete all duplicates such that each element appears only once. Return the linked list sorted as well.
+
+func removeDuplicatesFromSortedList(_ head: ListNode?) -> ListNode? {
+  guard var head else { return nil }
+
+  var storage: [Int: Int] = [:]
+  var current: ListNode? = head
+
+  // iterating thorugh the list to count the ocurrences of each value
   while current != nil {
-    count += 1
+    storage[current!.val, default: 0] += 1
     current = current!.next
   }
 
-  var targetIndex = count - n
-  // if target index is zero, skip first element
-  if targetIndex == 0 {
-    return head?.next
-  }
-
-  var index = 0
   current = head
   while current != nil {
-    index += 1
-    if index == targetIndex {
+    if storage[current!.val, default: 0] > 1 {
       current!.next = current!.next?.next
+      storage[current!.val, default: 1] -= 1
     } else {
       current = current!.next
     }
@@ -190,9 +181,82 @@ func removeNthFromEnd(_ head: ListNode?, _ n: Int) -> ListNode? {
   return head
 }
 
-removeNthFromEnd([1, 2, 3, 4, 5].toListNode, 2)?.toArray ?? []
-removeNthFromEnd([1].toListNode, 1)?.toArray ?? []
-removeNthFromEnd([1, 2].toListNode, 1)?.toArray ?? []
+removeDuplicatesFromSortedList([1, 1, 2].toListNode)?.toArray ?? []
+removeDuplicatesFromSortedList([1, 1, 2, 3, 3].toListNode)?.toArray ?? []
+
+// MARK: - 203. Remove Linked List Elements [Easy]
+// https://leetcode.com/problems/remove-linked-list-elements/
+// Given the head of a linked list and an integer val, remove all the nodes of the linked list that has Node.val == val, and return the new head.
+
+func removeLinkedListElements(_ head: ListNode?, _ val: Int) -> ListNode? {
+  guard var head else { return nil }
+
+  var current = head
+  while let next = current.next {
+    // skip the next node if it has the value we want to remove
+    if next.val == val {
+      current.next = next.next
+    } else {
+      current = next
+    }
+  }
+  // check if first node has the value we want to remove
+  return head.val == val ? head.next : head
+}
+
+removeLinkedListElements([1, 2, 6, 3, 4, 5, 6].toListNode, 6)?.toArray ?? []
+removeLinkedListElements([].toListNode, 1)?.toArray ?? []
+removeLinkedListElements([7, 7, 7, 7].toListNode, 7)?.toArray ?? []
+
+// MARK: - 206. Reverse Linked List [Easy]
+// https://leetcode.com/problems/reverse-linked-list/
+// Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+func reverseLinkedList(_ head: ListNode?) -> ListNode? {
+  guard var head else { return nil }
+  var tail = ListNode(head.val)
+  while let next = head.next {
+    tail = ListNode(next.val, tail)
+    head = next
+  }
+  return tail
+}
+
+reverseLinkedList([1, 2, 3, 4, 5].toListNode)?.toArray ?? []
+reverseLinkedList([1, 2].toListNode)?.toArray ?? []
+reverseLinkedList([].toListNode)?.toArray ?? []
+
+// MARK: - 234. Palindrome Linked List [Easy]
+// https://leetcode.com/problems/palindrome-linked-list/
+// Given the head of a singly linked list, return true if it is a palindrome or false otherwise.
+
+func palindromeLinkedList(_ head: ListNode?) -> Bool {
+  guard let head else { return true }
+
+  // reversing the list to get the tail
+  var current: ListNode? = head
+  var tail: ListNode? = ListNode(head.val)
+
+  while let next = current?.next {
+    tail = ListNode(next.val, tail)
+    current = next
+  }
+
+  // comparing the original list with the reversed one, for palindrome they should be the same
+  current = head
+  while tail != nil {
+    if current!.val != tail!.val {
+      return false
+    }
+    current = current!.next
+    tail = tail!.next
+  }
+
+  return true
+}
+
+palindromeLinkedList([1, 2, 2, 1].toListNode)
+palindromeLinkedList([1, 2].toListNode)
 
 // MARK: - 876. Middle of the Linked List [Easy]
 // https://leetcode.com/problems/middle-of-the-linked-list/
@@ -223,67 +287,3 @@ func middleNode(_ head: ListNode?) -> ListNode? {
 
 middleNode([1, 2, 3, 4, 5].toListNode)?.toArray ?? []
 middleNode([1, 2, 3, 4, 5, 6].toListNode)?.toArray ?? []
-
-// MARK: - 2. Add Two Numbers [Medium]
-// https://leetcode.com/problems/add-two-numbers/
-// You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit.
-// Add the two numbers and return the sum as a linked list.
-// You may assume the two numbers do not contain any leading zero, except the number 0 itself.
-
-func addTwoNumbers(_ l1: ListNode?, _ l2: ListNode?) -> ListNode? {
-  guard var l1, var l2 else { return nil }
-
-  var leftOver = (l1.val + l2.val) / 10
-  let head = ListNode((l1.val + l2.val) % 10)
-  var current = head
-
-  while l1.next != nil || l2.next != nil {
-    let value = (l1.next?.val ?? 0) + (l2.next?.val ?? 0) + leftOver
-
-    let newNode = ListNode(value % 10)
-    current.next = newNode
-    current = newNode
-
-    l1 = l1.next ?? l1
-    l2 = l2.next ?? l2
-    leftOver = value / 10
-  }
-
-  if leftOver > 0 {
-    let newNode = ListNode(leftOver)
-    current.next = newNode
-    current = newNode
-  }
-
-  return head
-}
-
-addTwoNumbers([2, 4, 3].toListNode, [5, 6, 4].toListNode)?.toArray ?? []
-addTwoNumbers([0].toListNode, [0].toListNode)?.toArray ?? []
-addTwoNumbers([9, 9, 9, 9, 9, 9, 9].toListNode, [9, 9, 9, 9].toListNode)?.toArray ?? []
-
-// MARK: - 24. Swap Nodes in Pairs [Medium]
-// https://leetcode.com/problems/swap-nodes-in-pairs/
-// Given a linked list, swap every two adjacent nodes and return its head.
-// You must solve the problem without modifying the values in the list's nodes (i.e., only nodes themselves may be changed.)
-
-func swapPairs(_ head: ListNode?) -> ListNode? {
-  guard var current = head, current.next != nil else { return head }
-
-  while let next = current.next {
-    // swapping values without temp variable
-    current.val += next.val
-    next.val = current.val - next.val
-    current.val -= next.val
-
-    // hopping over the next node
-    current = next.next ?? next
-  }
-
-  return head
-}
-
-swapPairs([1, 2, 3, 4].toListNode)?.toArray ?? []
-swapPairs([1, 2, 3].toListNode)?.toArray ?? []
-swapPairs([1].toListNode)?.toArray ?? []
-swapPairs([].toListNode)?.toArray ?? []
